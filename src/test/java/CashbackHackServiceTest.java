0cfafaa74a5d2dab8a8ff2af6b36cf8ad4074a49
fdarcy <fdarcy@mail.ru>
2024-01-12 16:42:54 +0300

    testng change

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -10,7 +10,7 @@ public class CashbackHackServiceTest {
 
         int actual = service.remain(1500);
         int expected = 500;
-        Assertions.assertEquals(actual, expected);
+        Assertions.assertEquals(expected, actual);
 
     }
 
@@ -20,7 +20,7 @@ public class CashbackHackServiceTest {
 
         int actual = service.remain(0);
         int expected = 1000;
-        Assertions.assertEquals(actual, expected);
+        Assertions.assertEquals(expected, actual);
 
     }
 
@@ -30,7 +30,7 @@ public class CashbackHackServiceTest {
 
         int actual = service.remain(1000);
         int expected = 0;
-        Assertions.assertEquals(actual, expected);
+        Assertions.assertEquals(expected, actual);
 
     }
 }
